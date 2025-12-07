40b6df70fdad8221ab01e53996e296d4cb53f294
Kamil Hurajt <hurajtk@gmail.com>
2025-12-07 23:12:16 +0100

    FIx types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-export * from './components/Invoice';
-export { InvoiceAmount } from './components/Invoice/InvoiceRow/Amount/InvoiceAmount';
-export type {
-    VatRoundingCallback,
-    InvoiceAmountInputProps,
-    InvoiceAmountValueProps,
-    VatRoundingStrategy
-} from './components/Invoice/InvoiceRow/Amount/types';
+export * from './components/Amount';
